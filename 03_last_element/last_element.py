acfaea16cf0fedc966de13a1f13c3bd0ff413e6b
root <midnightlunalux@gmail.com>
2022-11-09 20:18:02 -0500

    adding completed files

---
+++ 03_last_element/last_element.py
@@ -0,0 +1,10 @@
+def last_element(lst):
+    """Return last item in list (None if list is empty.
+    
+        >>> weekday_name(9)
+        3
+        
+        >>> last_element([]) is None
+        True
+    """
+    return lst[- 1]
